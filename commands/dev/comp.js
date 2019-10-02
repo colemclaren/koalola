4720ef90944f6990811cafd184e8f46010afa8d3
IcyMonarch <37213221+IcyMonarch@users.noreply.github.com>
2019-10-01 21:49:41 -0400

    attempt to fix comp for leo

--- commands/dev/comp.js
+++ commands/dev/comp.js
@@ -8,7 +8,7 @@ module.exports = class Comp extends commando.Command {
 			aliases: ['compensate', 'c'],
 			memberName: 'comp',
 			description: 'Respond to a compensation ticket.',
-			ownerOnly: true,
+			ownerOnly: false,
 			args: [{
 				key: 'id',
 				label: 'CompID',
